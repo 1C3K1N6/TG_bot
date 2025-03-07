2b7dcfc3bfae0181bccbd7a053509e9f7878116c
1C3K1N6 <fearengine@yandex.ru>
2025-03-07 19:00:17 +0500

    Start quiz

--- DB_quiz.py
+++ DB_quiz.py
@@ -1,12 +1,2 @@
-import aiosqlite
 
-async def create_table():
-    # Создаем соединение с базой данных (если она не существует, то она будет создана)
-    async with aiosqlite.connect('quiz_bot.db') as db:
-        # Выполняем SQL-запрос к базе данных
-        await db.execute('''CREATE TABLE IF NOT EXISTS quiz_state (user_id INTEGER PRIMARY KEY, question_index INTEGER)''')
-        # Сохраняем изменения
-        await db.commit()
 
-# Запускаем создание таблицы базы данных
-await create_table()
