9fa7b3669a2d75bf81fa25019bda29faf0908cae
1C3K1N6 <fearengine@yandex.ru>
2025-03-12 21:12:08 +0500

    questions in other file (DB_qiuz)

--- Start.py
+++ Start.py
@@ -7,6 +7,7 @@ from aiogram.filters.command import Command
 from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
 from aiogram import F
 from DB_quiz import quiz_data
+import csv
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
